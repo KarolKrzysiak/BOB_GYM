4dc35ffb1ea0bb7527dea890a48a60e5c19de600
bob <kkrzysiak91@gmail.com>
2025-02-11 17:27:30 +0100

    bob_begin

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ root = Tk()
 frm = tk.Frame(root)
 frm.grid()
 tk.Label(frm, text="BOB GYM").grid(column=0,row=0)
-#tk.Button(frm, text="Quit", command=root.destroy).grid(column=1,row=1)
+tk.Button(frm, text="Quit", command=root.destroy).grid(column=1,row=1)
 
 tk.Button(frm, text="Analytics").grid(column=1,row=1)
 tk.Button(frm, text="Charts").grid(column=2,row=1)
